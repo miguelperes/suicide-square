610705f7d2e4ede8cf0870a7c5dd9b64057f8569
Miguel Péres <foxmpc@gmail.com>
2017-02-06 22:23:24 -0200

    Add warning for missing spawnPosition Transform component in PlayerShoot

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -16,12 +16,12 @@ public class PlayerInputManager : MonoBehaviour {
 
 	void Update () {
 		if ( Input.GetKeyDown(KeyCode.Space) ) {
-			Quaternion currentRotation = gameObject.transform.rotation;
-			Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
-			playerShoot.fire(rotation);
-			Debug.Log("Player position"+gameObject.transform.position);
+			//TODO
+			Quaternion playerFront = gameObject.transform.rotation;
+			playerShoot.fire(playerFront);
 		} 
 		else if( Input.GetKeyDown(KeyCode.RightArrow) ) {
+			//TODO
 			Quaternion currentRotation = gameObject.transform.rotation;
 			Quaternion rotation = Quaternion.Euler(0f, 0f, gameObject.transform.rotation.z + -90f);
 			playerShoot.fire(rotation);
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,20 +4,28 @@ public class PlayerShoot : MonoBehaviour {
 
 	[SerializeField] float speed = 1f;
 	[SerializeField] float timeToLive = 5f;
+	[SerializeField] Transform spawnPosition;
 
 	public GameObject bulletPrefab;
-	public Transform spawnTransform;
+
+	void Start() {
+		if( spawnPosition == null) {
+			Debug.LogWarning("<b>Spawn position</b> for player's bullets <b>is not set</b>", spawnPosition);
+		}
+	}
 	
 	public void fire(Quaternion direction) {
-		var bulletInstance = (GameObject) Instantiate( bulletPrefab, spawnTransform.position, direction );
-
-		Debug.Log("bullet instance pos: " + spawnTransform.position);
-		bulletInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, speed);
+		Debug.Log("<i>" + spawnPosition.position + "</i>");
+		var bulletInstance = (GameObject) Instantiate( bulletPrefab, spawnPosition.position, direction );
+		Debug.Log("<i>" + bulletInstance.transform.position + "</i>");
+		// bulletInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, speed);
 		// bulletInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, speed), ForceMode2D.Impulse);
 
 		Destroy(bulletInstance, timeToLive);
 	}
 
 	void Update()
-	{	}
+	{
+		Debug.Log("<b>" + spawnPosition.position + "</b>");
+	}
 }
