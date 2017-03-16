ad147fa8eb051ced64fae32738e1f0506cb31248
Miguel Péres <foxmpc@gmail.com>
2017-03-16 20:56:42 -0300

    Shrink player sprite size as they shoot

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -6,9 +6,13 @@ public class PlayerShoot : MonoBehaviour {
 	[SerializeField] float timeToLive = 5f;
 	[SerializeField] Transform spawnPosition;
 
+	BodyShrink bodyShrink;
+
 	public GameObject bulletPrefab;
 
 	void Start() {
+		bodyShrink = GetComponent<BodyShrink>();
+		
 		if( spawnPosition == null) {
 			Debug.LogWarning("<b>Spawn position</b> for player's bullets <b>is not set</b>", spawnPosition);
 		}
@@ -17,6 +21,7 @@ public class PlayerShoot : MonoBehaviour {
 	public void fire(Vector2 direction) {
 		var bulletInstance = (GameObject) Instantiate( bulletPrefab, spawnPosition.position, spawnPosition.rotation );
 		bulletInstance.GetComponent<Rigidbody2D>().AddForce(direction * speed, ForceMode2D.Impulse);
+		bodyShrink.shrink();
 		
 		Destroy(bulletInstance, timeToLive);
 	}
