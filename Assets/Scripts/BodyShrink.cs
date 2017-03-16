621c4786bde96cb4f987da2383467b490fa5f7d7
Miguel Péres <foxmpc@gmail.com>
2017-03-16 20:54:47 -0300

    Add script to control player size

---
+++ Assets/Scripts/BodyShrink.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BodyShrink : MonoBehaviour {
+
+	[SerializeField] Transform transformObject;
+	[SerializeField] float maxSize;
+	[SerializeField] float minSize;
+	[SerializeField] float step;
+
+	Vector2 currentSize;
+	Vector2 modifier;
+
+	void Start () {
+		currentSize = transformObject.localScale;		
+		modifier = new Vector2(step, step);
+	}
+
+	public void shrink() {
+		if(currentSize.x > minSize) {
+			currentSize -= modifier;
+			transformObject.localScale = currentSize;
+		}
+	}
+
+	public void expand() {
+		if(currentSize.x < maxSize) {
+			currentSize += modifier;
+			transformObject.localScale = currentSize;
+		}
+	}
+
+	void Update(){
+	}
+}
