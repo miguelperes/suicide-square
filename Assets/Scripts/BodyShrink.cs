796d422c9b9648e8595d2dd32b2e6b9485094437
Miguel Péres <foxmpc@gmail.com>
2017-04-27 22:54:43 -0300

    Shrink with tweening animation

--- Assets/Scripts/BodyShrink.cs
+++ Assets/Scripts/BodyShrink.cs
@@ -8,20 +8,28 @@ public class BodyShrink : MonoBehaviour {
 	[SerializeField] float minSize;
 	[SerializeField] float step;
 	[SerializeField] bool enable = true;
+	[SerializeField] float inflateRate = 0.5f;
 
 	Vector2 currentSize;
 	Vector2 modifier;
+
+
 
 	void Start () {
 		currentSize = transformObject.localScale;		
 		modifier = new Vector2(step, step);
 	}
 
+	void callback() {
+		Debug.Log("callback");
+	}
+
 	public void shrink() {
 		if(enable && currentSize.x > minSize) {
+			transform.DOScale((currentSize.x + step), 0.2f)
+				.OnComplete( () => transform.DOScale((currentSize.x - step), 0.05f) );
+
 			currentSize -= modifier;
-			transformObject.localScale = currentSize;
-			// transform.DOScale(new Vector3(5, 5, 5), 5.0f);
 		}
 	}
 
@@ -30,6 +38,11 @@ public class BodyShrink : MonoBehaviour {
 			currentSize += modifier;
 			transformObject.localScale = currentSize;
 		}
+	}
+
+	private void inflateAnimation() {
+		float sizeToInflate = currentSize.x + inflateRate;
+		transform.DOScale(sizeToInflate, 0.5f);
 	}
 	
 }
